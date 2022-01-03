3b766f8b28c5572556468ca461ed34bedf9047a1
gryder8 <gavin.ryder82@gmail.com>
2022-01-02 19:34:54 -0800

    Initial Commit

---
+++ MLB The Show Flips/MLB_The_Show_FlipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLB_The_Show_FlipsApp.swift
+//  MLB The Show Flips
+//
+//  Created by Gavin Ryder on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MLB_The_Show_FlipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
